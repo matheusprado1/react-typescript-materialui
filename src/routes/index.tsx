7a2077c215dce6606308c27ca1c5d95d26920e8e
matheusprado1 <matheusprado1096@gmail.com>
2025-05-07 14:22:28 -0300

    feat: implement theme context and toggle functionality with dark mode support

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,16 @@
 import { Button } from '@mui/material';
 import { Routes, Route, Navigate } from 'react-router';
+import { useAppThemeContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
+  const { toggleTheme } = useAppThemeContext();
+
   return (
     <Routes>
       <Route
         path="/pagina-inicial"
         element={
-          <Button variant="contained" color="primary">
+          <Button variant="contained" color="primary" onClick={toggleTheme}>
             Teste
           </Button>
         }
