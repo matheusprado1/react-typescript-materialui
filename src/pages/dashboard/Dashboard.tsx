a45259c47b1da53b406a0416299ddc2a755c2201
matheusprado1 <matheusprado1096@gmail.com>
2025-05-08 16:47:23 -0300

    feat: implement Dashboard page and integrate with routing and layout

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,5 @@
+import { PageLayoutBase } from '../../shared/layouts/PageBaseLayout';
+
+export const Dashboard = () => {
+  return <PageLayoutBase title="Página inicial">Testando</PageLayoutBase>;
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router';
-import { Button } from '@mui/material';
 
 import { useDrawerContext } from '../shared/contexts';
+import { Dashboard } from '../pages';
 
 export const AppRoutes = () => {
-  const { toggleDrawerOpen, setDrawerOptions } = useDrawerContext();
+  const { setDrawerOptions } = useDrawerContext();
 
   useEffect(() => {
     setDrawerOptions([
@@ -24,14 +24,7 @@ export const AppRoutes = () => {
 
   return (
     <Routes>
-      <Route
-        path="/pagina-inicial"
-        element={
-          <Button variant="contained" color="primary" onClick={toggleDrawerOpen}>
-            Toggle Drawer
-          </Button>
-        }
-      />
+      <Route path="/pagina-inicial" element={<Dashboard />} />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
