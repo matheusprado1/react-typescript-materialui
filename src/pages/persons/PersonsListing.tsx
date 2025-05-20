56fd2427550ba816be9d155e58f5adb2e6b03be1
matheusprado1 <matheusprado1096@gmail.com>
2025-05-20 13:15:50 -0300

    feat: add useDebounce hook and integrate it into PersonsListing component

--- src/pages/persons/PersonsListing.tsx
+++ src/pages/persons/PersonsListing.tsx
@@ -1,25 +1,30 @@
 import { useEffect, useMemo } from 'react';
 import { useSearchParams } from 'react-router';
 
+import { PersonsService } from '../../shared/services/api/persons/PersonsService';
 import { ListingTools } from '../../shared/components';
 import { PageLayoutBase } from '../../shared/layouts';
-import { PersonsService } from '../../shared/services/api/persons/PersonsService';
+import { useDebounce } from '../../shared/hooks';
 
 export const PersonsListing: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce(3000, false);
 
   const search = useMemo(() => {
     return searchParams.get('search') || '';
   }, [searchParams]);
 
   useEffect(() => {
-    PersonsService.getAll(1, search).then(result => {
-      if (result instanceof Error) {
-        alert(result.message);
-      } else {
-        console.log(result);
-      }
+    debounce(() => {
+      PersonsService.getAll(1, search).then(result => {
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
+          console.log(result);
+        }
+      });
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [search]);
 
   return (
