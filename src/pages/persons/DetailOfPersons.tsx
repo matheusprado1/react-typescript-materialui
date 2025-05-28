5088903534765beb845ae73db3043fa3b4568fc6
matheusprado1 <matheusprado1096@gmail.com>
2025-05-28 15:02:57 -0300

    feat: refactor DetailOfPersons component layout and add cityId field

--- src/pages/persons/DetailOfPersons.tsx
+++ src/pages/persons/DetailOfPersons.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router';
-import { Button, LinearProgress, TextField } from '@mui/material';
+import { Box, Grid, LinearProgress, Paper, TextField, Typography } from '@mui/material';
 import { Controller, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -53,7 +53,7 @@ export const DetailOfPersons: React.FC = () => {
           alert(result.message);
           navigate('/persons');
         } else {
-          setName(result.firstName + ' ' + result.lastName);
+          setName(result.firstName);
 
           setValue('firstName', result.firstName);
           setValue('lastName', result.lastName);
@@ -74,6 +74,7 @@ export const DetailOfPersons: React.FC = () => {
         if (result instanceof Error) {
           alert(result.message);
         } else {
+          setName(data.firstName);
           navigate(`/persons/detail/${result}`);
         }
       });
@@ -83,6 +84,8 @@ export const DetailOfPersons: React.FC = () => {
 
         if (result instanceof Error) {
           alert(result.message);
+        } else {
+          setName(data.firstName);
         }
       });
     }
@@ -118,49 +121,99 @@ export const DetailOfPersons: React.FC = () => {
         />
       }
     >
-      {isLoading && <LinearProgress variant="indeterminate" />}
+      {/* {isLoading && <LinearProgress variant="indeterminate" />} */}
 
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Controller
-          name="firstName"
-          control={control}
-          render={({ field }) => (
-            <TextField
-              label="Nome"
-              fullWidth
-              {...field}
-              error={!!errors.firstName}
-              helperText={errors.firstName?.message}
-            />
-          )}
-        />
-        <Controller
-          name="lastName"
-          control={control}
-          render={({ field }) => (
-            <TextField
-              label="Sobrenome"
-              fullWidth
-              {...field}
-              error={!!errors.lastName}
-              helperText={errors.lastName?.message}
-            />
-          )}
-        />
-        <Controller
-          name="email"
-          control={control}
-          render={({ field }) => (
-            <TextField
-              label="Email"
-              type="email"
-              fullWidth
-              {...field}
-              error={!!errors.email}
-              helperText={errors.email?.message}
-            />
-          )}
-        />
+        <Box margin={1} display="flex" flexDirection="column" component={Paper} variant="outlined">
+          <Grid container direction="column" padding={2} spacing={2}>
+            {isLoading && (
+              <Grid>
+                <LinearProgress variant="indeterminate" />
+              </Grid>
+            )}
+
+            <Grid>
+              <Typography variant="h6">Geral</Typography>
+            </Grid>
+
+            <Grid container direction="row">
+              <Grid size={{ xs: 12, sm: 12, md: 6, lg: 4, xl: 2 }}>
+                <Controller
+                  name="firstName"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      label="Nome"
+                      fullWidth
+                      {...field}
+                      error={!!errors.firstName}
+                      helperText={errors.firstName?.message}
+                      disabled={isLoading}
+                    />
+                  )}
+                />
+              </Grid>
+            </Grid>
+
+            <Grid container direction="row">
+              <Grid size={{ xs: 12, sm: 12, md: 6, lg: 4, xl: 2 }}>
+                <Controller
+                  name="lastName"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      label="Sobrenome"
+                      fullWidth
+                      {...field}
+                      error={!!errors.lastName}
+                      helperText={errors.lastName?.message}
+                      disabled={isLoading}
+                    />
+                  )}
+                />
+              </Grid>
+            </Grid>
+
+            <Grid container direction="row">
+              <Grid size={{ xs: 12, sm: 12, md: 6, lg: 4, xl: 2 }}>
+                <Controller
+                  name="email"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      label="Email"
+                      type="email"
+                      fullWidth
+                      {...field}
+                      error={!!errors.email}
+                      helperText={errors.email?.message}
+                      disabled={isLoading}
+                    />
+                  )}
+                />
+              </Grid>
+            </Grid>
+
+            <Grid container direction="row">
+              <Grid size={{ xs: 12, sm: 12, md: 6, lg: 4, xl: 2 }}>
+                <Controller
+                  name="cityId"
+                  control={control}
+                  render={({ field }) => (
+                    <TextField
+                      label="Cidade"
+                      fullWidth
+                      {...field}
+                      error={!!errors.cityId}
+                      helperText={errors.cityId?.message}
+                      disabled={isLoading}
+                    />
+                  )}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Box>
       </form>
     </PageLayoutBase>
   );
